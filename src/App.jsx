8db0e57361624c6614c2f1bd364efa3651ddf04a
ella-quintin <equintin41@gmail.com>
2025-07-30 14:59:25 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Landing from "./pages/landing"
+
+const router = createBrowserRouter([
+  {
+    path: "/", element: <Landing />
+  }
+])
+
+const App = () => {
+  return (
+    <RouterProvider router={router} />
+  )
+}
+
+export default App
